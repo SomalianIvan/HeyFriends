34d194046c840bc5f3ff9e4eda540503446e88ba
Timur Borkhodoev <timur.borkhodov@mail.utoronto.ca>
2019-07-21 15:06:10 -0400

    feat: add 'hey friends' replies

---
+++ bot.py
@@ -0,0 +1,23 @@
+from telegram.ext import InlineQueryHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+updater = Updater(token='646524007:AAHMlqmKmGg-00Im4b8WuzI-AaxlvtLvF6o', use_context=True)
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+def echo(update, context):
+    """Echo the user message."""
+    if "Hey friends," in update.message.text:
+        update.message.reply_text("What a great idea. How about today?")
+
+dispatcher = updater.dispatcher
+dispatcher.add_handler(MessageHandler(Filters.text, echo))
+
+updater.start_polling()
